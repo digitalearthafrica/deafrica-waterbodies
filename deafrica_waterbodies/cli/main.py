d2046529d0b12bedbd7d08de7a4fbfe5c8bc9962
vikineema <victoria@kartoza.com>
2023-11-08 22:58:18 +0300

    Update timeseries cli

--- deafrica_waterbodies/cli/main.py
+++ deafrica_waterbodies/cli/main.py
@@ -4,6 +4,7 @@ import deafrica_waterbodies.__version__
 from deafrica_waterbodies.cli.generate_polygons import generate_polygons
 from deafrica_waterbodies.cli.generate_timeseries import generate_timeseries
 
+
 @click.version_option(package_name="deafrica_waterbodies", version=deafrica_waterbodies.__version__)
 @click.group(help="Run deafrica-waterbodies.")
 def main():
